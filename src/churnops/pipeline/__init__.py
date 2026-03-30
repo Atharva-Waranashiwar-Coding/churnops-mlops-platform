15a9b44cf9afaca4fa9a645d126d2663614706ee
Atharva Waranashiwar <waranashiwaratharva@gmail.com>
2026-03-30 11:17:10 -0400

    refactor: split training workflow into dedicated pipeline modules

--- src/churnops/pipeline/__init__.py
+++ src/churnops/pipeline/__init__.py
@@ -1,1 +1,5 @@
-"""Pipeline entrypoints for ChurnOps."""
+"""Pipeline orchestration interfaces for ChurnOps."""
+
+from churnops.pipeline.runner import TrainingPipelineResult, run_local_training
+
+__all__ = ["TrainingPipelineResult", "run_local_training"]
